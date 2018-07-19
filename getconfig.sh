fc533d079795b5c479bd3e6ffc18d63f4e638b22
Christophe Graniczny <getintouch@christophegraniczny.com>
2018-07-19 18:11:01 +0100

    Exit the case statement properly

--- getconfig.sh
+++ getconfig.sh
@@ -1,62 +1,58 @@
 #!/bin/bash
-set -e
+set -eux
 
 function getfile () {
-  filename="$1"
-  if [ -f filename ]; then
-    mv "$filename" "$filename-backup"
-  fi
-  wget https://raw.githubusercontent.com/krzystof/configs/master/$1 "$filename"
+  wget https://raw.githubusercontent.com/krzystof/configs/master/"$1"
 }
 
 case "$1" in
   "babel" )
     getfile ".babelrc"
-    ;&
+    ;;
 
   "editorconfig" )
     getfile ".editorconfig"
-    ;&
+    ;;
 
   "eslint" )
     getfile ".eslintrc.js"
-    ;&
+    ;;
 
   "jest" )
-    getfile ".jest.config.js"
-    ;&
+    getfile "jest.config.js"
+    ;;
 
   "prettier" )
     getfile ".prettierrc"
-    ;&
+    ;;
 
   "projections" )
     getfile ".projections.json"
-    ;&
+    ;;
 
   "phpcs" )
     getfile "phpcs.xml"
-    ;&
+    ;;
 
   "phpcsfixer" )
     getfile ".php_cs"
-    ;&
+    ;;
 
   "phpmd" )
     getfile "phpmd.xml"
-    ;&
+    ;;
 
   "phpunit" )
     getfile "phpunit.xml"
-    ;&
+    ;;
 
   "tsconfig" )
     getfile "tsconfig.json"
-    ;&
+    ;;
 
   "tslint" )
     getfile "tslint.json"
-    ;&
+    ;;
 
   "" )
     echo
